3fbb8b2c43394b246c6b5b2e50b723a1b9a21681
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2020-08-07 13:05:41 +0200

    Create arti_eksi_sifir_mi.py

---
+++ arti_eksi_sifir_mi.py
@@ -0,0 +1,10 @@
+num1=int(input('Lütfen bir sayi giriniz'))
+
+if num1 ==0:
+    print(f'{num1} sayisi 0 dir.')
+
+elif num1>0:
+    print(f'{num1} sayisi poziftir.')
+
+else:
+    print(f'{num1} sayisi negatiftir.')
